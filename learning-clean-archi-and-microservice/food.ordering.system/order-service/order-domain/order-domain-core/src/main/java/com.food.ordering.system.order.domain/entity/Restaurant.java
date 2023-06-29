fbc4d1d17167ffc77dc815a9f0dc2cd258f7d089
Arnaud NDIO <arnaud.ndioazom@gmail.com>
2023-06-29 08:09:48 +0200

    implementing input ports in order application service

--- learning-clean-archi-and-microservice/food.ordering.system/order-service/order-domain/order-domain-core/src/main/java/com.food.ordering.system.order.domain/entity/Restaurant.java
+++ learning-clean-archi-and-microservice/food.ordering.system/order-service/order-domain/order-domain-core/src/main/java/com.food.ordering.system.order.domain/entity/Restaurant.java
@@ -23,6 +23,7 @@ public class Restaurant extends AggregateRoot<RestaurantId> {
         return active;
     }
 
+
     public static final class Builder {
         private RestaurantId restaurantId;
         private List<Product> products;
@@ -35,7 +36,7 @@ public class Restaurant extends AggregateRoot<RestaurantId> {
             return new Builder();
         }
 
-        public Builder id(RestaurantId val) {
+        public Builder restaurantId(RestaurantId val) {
             restaurantId = val;
             return this;
         }
